915128b6afcdda5a30be3c76eeccc6e3ba9e9045
savio barros <saviobarr@gmail.com>
2016-03-15 22:12:00 -0300

    first commit

---
+++ AngularJsByExampleBook/WebContent/js/controller.js
@@ -0,0 +1,18 @@
+angular.module('app',[])
+.controller('GuessTheNumberController',GuessTheNumberController);
+function GuessTheNumberController($scope){
+	$scope.verifyGuess = function(){
+		$scope.deviation = $scope.original - $scope.guess1;
+		$scope.noOfTries = $scope.noOfTries + 1;
+	}
+	
+	$scope.initializeGame = function(){
+		$scope.noOfTries = 0;
+		$scope.original = Math.floor((Math.random() * 1000) + 1);
+		$scope.guess1 = null;
+		$scope.deviation = null;
+	}
+	
+	$scope.initializeGame();
+	
+}
